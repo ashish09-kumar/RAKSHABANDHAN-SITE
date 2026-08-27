00a4860684754cfa9ea120174a40befda34aa28f
ashish09-kumar <kuashish432@gmail.com>
2026-08-27 21:51:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function showMessage() {
+  document.getElementById("message").innerHTML =
+    "💖 Dear  ABHA MERCY PERBHA AND NEHA, May this Raksha Bandhan bring happiness, protection, and endless love! 💖";
+}
